9787eb546343dcedcd3c5511b57e9c9305c5be73
Maciej <gorski.maciej@o2.pl>
2021-08-29 16:33:21 +0200

    added simple achievement popup prefab and code for it

---
+++ Assets/Scripts/UI/MainOnionButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainOnionButton : MonoBehaviour
+{
+    private Button button;
+    private ParticleSystem buttonParticleSystem; //launch single burst on click
+    private Animator onionAnimator; //reduce size and increase it back on click
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
